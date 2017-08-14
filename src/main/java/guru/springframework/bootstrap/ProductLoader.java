de414230ca1eca16c98ae2d43f17c5880dc0f382
saintur.b <saintur.b@itzone.mn>
2017-08-14 18:32:22 +0800

    update

--- src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -1,8 +1,6 @@
 package guru.springframework.bootstrap;
 
-import guru.springframework.domain.Product;
 import guru.springframework.domain.Plate;
-import guru.springframework.repositories.ProductRepository;
 import guru.springframework.repositories.PlateRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +14,9 @@ import java.util.Date;
 @Component
 public class ProductLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private ProductRepository productRepository;
     private PlateRepository plateRepository;
 
     private Logger log = Logger.getLogger(ProductLoader.class);
-
-    @Autowired
-    public void setProductRepository(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     @Autowired
     public void setPlateRepository(PlateRepository plateRepository) {
@@ -33,22 +25,6 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-
-        Product shirt = new Product();
-        shirt.setDescription("Spring Framework TTT");
-        shirt.setPrice(new BigDecimal("18.95"));
-        shirt.setImageUrl("https://springframework.com/wp-content/uploads/2015/04/rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setProductId("235268845711068308");
-        productRepository.save(shirt);
-
-        log.info("Saved Shirt - id: " + shirt.getId());
-
-        Product mug = new Product();
-        mug.setDescription("Spring FrameworkMug");
-        mug.setImageUrl("https://springframework.com/wp-content/uploads/2015/04/r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setProductId("168639393495335947");
-        mug.setPrice(new BigDecimal("11.95"));
-        productRepository.save(mug);
 
         // for(int i=0;i<10;i++){
         //     Plate one = new Plate();
